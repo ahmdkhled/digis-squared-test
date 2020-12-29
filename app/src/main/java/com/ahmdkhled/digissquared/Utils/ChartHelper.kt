b3560cb8f8be46b372f4ffc163b851d1d2fc632c
ahmdkhled <ahmdkhled@gmail.com>
2020-12-29 06:09:15 +0200

    show signal values of first chart

---
+++ app/src/main/java/com/ahmdkhled/digissquared/Utils/ChartHelper.kt
@@ -0,0 +1,122 @@
+package com.ahmdkhled.digissquared.Utils
+
+import android.graphics.Color
+import com.ahmdkhled.digissquared.R
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.IAxisValueFormatter
+import com.github.mikephil.charting.formatter.ValueFormatter
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.math.min
+
+class ChartHelper {
+     var dataSets=ArrayList<LineDataSet>()
+    init {
+        dataSets.add(createSet("RSRP",Color.BLUE))
+        dataSets.add(createSet("RSRQ",Color.RED))
+        dataSets.add(createSet("SINR",Color.GREEN))
+
+    }
+
+    private fun createSet(label:String,color:Int): LineDataSet {
+        val set = LineDataSet(null, label)
+        set.axisDependency = YAxis.AxisDependency.LEFT
+        set.color = color
+        set.setCircleColor(Color.WHITE)
+        set.lineWidth = 4f
+        set.circleRadius = 4f
+        set.fillAlpha = 65
+        set.fillColor = Color.GREEN
+        set.highLightColor = Color.rgb(244, 117, 117)
+        set.valueTextColor = Color.WHITE
+        set.valueTextSize = 9f
+        set.setDrawValues(false)
+        return set
+    }
+
+    fun setupGraph(graph:LineChart,min:Float,max:Float){
+
+        graph.setTouchEnabled(true)
+        graph.setDragEnabled(true)
+        graph.setScaleEnabled(true)
+        graph.setDrawGridBackground(false)
+        graph.setPinchZoom(true)
+        graph.setBackgroundColor(Color.GRAY)
+        val data = LineData()
+        data.setValueTextColor(Color.WHITE)
+        graph.setData(data)
+        graph.getDescription().setText("time")
+        graph.getDescription().setTextColor(Color.WHITE)
+
+        // get the legend (only possible after setting data)
+
+        // get the legend (only possible after setting data)
+//        val l: Legend = memGraph.getLegend()
+//
+//        // modify the legend ...
+//
+//        // modify the legend ...
+//        l.form = Legend.LegendForm.LINE
+//        l.textColor = Color.WHITE
+
+        val xl: XAxis = graph.getXAxis()
+        xl.position=XAxis.XAxisPosition.BOTTOM_INSIDE
+        xl.textColor = Color.WHITE
+        xl.setDrawGridLines(false)
+        xl.setAvoidFirstLastClipping(true)
+        xl.isEnabled = true
+        xl.valueFormatter=MyXAxisValueFormatter()
+
+        //signal axis |
+        val leftAxis: YAxis = graph.getAxisLeft()
+        leftAxis.textColor = Color.WHITE
+        leftAxis.axisMaximum = -60f
+        leftAxis.axisMinimum = -140f
+        leftAxis.setDrawGridLines(true)
+        leftAxis.gridColor = Color.WHITE
+
+        val rightAxis: YAxis = graph.getAxisRight()
+        rightAxis.isEnabled = false
+
+        val mData=LineData(dataSets[0],dataSets[1],dataSets[2])
+        graph.data=mData
+    }
+
+    fun addEntry(graph: LineChart,value :Float,index:Int) {
+
+        val data=graph.data
+        if (data==null)return
+
+        var set = data.getDataSetByIndex(0)
+
+        data.addEntry(
+            Entry(set.entryCount.toFloat(),value ),index
+        )
+        data.notifyDataChanged()
+        // let the graph know it's data has changed
+        graph.notifyDataSetChanged()
+
+        // limit the number of visible entries
+        //graph.setVisibleXRangeMaximum(120F)
+
+        // move to the latest entry
+        graph.moveViewToX(data.entryCount.toFloat())
+    }
+
+
+    class MyXAxisValueFormatter : ValueFormatter() {
+        override fun getFormattedValue(value: Float): String {
+            val hour=Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+            val minute=Calendar.getInstance().get(Calendar.MINUTE)
+            return "$hour:$minute"
+        }
+
+    }
+}
--- app/src/main/java/com/ahmdkhled/digissquared/view/MainActivity.kt
+++ app/src/main/java/com/ahmdkhled/digissquared/view/MainActivity.kt
@@ -1,39 +1,58 @@
 package com.ahmdkhled.digissquared.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
-import androidx.lifecycle.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.ahmdkhled.digissquared.App
 import com.ahmdkhled.digissquared.Network.Api
 import com.ahmdkhled.digissquared.R
-import com.ahmdkhled.digissquared.repo.RandomNumsRepo
+import com.ahmdkhled.digissquared.Utils.ChartHelper
+import com.ahmdkhled.digissquared.databinding.ActivityMainBinding
 import com.ahmdkhled.digissquared.viewModel.MainActivityVM
 import com.ahmdkhled.digissquared.viewModel.MainActivityVMFactory
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.channels.Channel
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.math.sign
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
 
-     lateinit var mainActivityVM: MainActivityVM
+    lateinit var binding:ActivityMainBinding
+    lateinit var mainActivityVM: MainActivityVM
     @Inject lateinit var factory:MainActivityVMFactory
     @Inject lateinit var api: Api
+    lateinit var chartHelper:ChartHelper
     private  val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
         (application as App).mainActivityComponent.inject(this)
-
         mainActivityVM=ViewModelProvider(this,factory).get(MainActivityVM::class.java)
 
+        chartHelper=ChartHelper()
 
 
+
+
+
+        chartHelper.setupGraph(binding.chart1,0f,0f)
         pullFromServer()
+
 
 
 
@@ -43,8 +62,19 @@ class MainActivity : AppCompatActivity() {
     fun getSignalValues(){
         val lifecycleOwner:LifecycleOwner=this
         mainActivityVM.getRandomNumbers()
-            .observe(lifecycleOwner, Observer {
-                Log.d(TAG, "onCreate: $it")
+            .observe(lifecycleOwner, Observer {res->
+                Log.d(TAG, "onCreate: $res")
+
+                if (res.success&&!res.loading){
+                    val signalRes=res.res
+                    if (signalRes?.RSRP!=null)
+                    chartHelper.addEntry(binding.chart1, signalRes.RSRP.toFloat(),0)
+                    if (signalRes?.RSRQ!=null)
+                    chartHelper.addEntry(binding.chart1,signalRes.RSRQ.toFloat(),1)
+                    if (signalRes?.SINR!=null)
+                    chartHelper.addEntry(binding.chart1,signalRes.SINR.toFloat(),2)
+                }
+
             })
     }
 
@@ -56,12 +86,15 @@ class MainActivity : AppCompatActivity() {
                     break
                 }else{
                     Log.d(TAG, "repeate: ")
+                    getSignalValues()
                     delay(2000)
                 }
 
             }
         }
     }
+
+
 
     override fun onDestroy() {
         super.onDestroy()
